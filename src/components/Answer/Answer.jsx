4aa3d6b7af05d40670375a81d3f4a1463422838a
Arpita Behura <arpitabehura.009@gmail.com>
2024-05-02 21:29:49 +0200

    Adding color codes for correct/wrong answer

--- src/components/Answer/Answer.jsx
+++ src/components/Answer/Answer.jsx
@@ -1,19 +1,23 @@
 import './Answer.css';
+import {useRef} from "react";
 
 export default function Answer({eachOption, index,optionDisabled, isAnswerOptionDisabled}){
     const classNames = `answer ${isAnswerOptionDisabled ? 'disabled-div' : ''}`;
+    const optionRef = useRef(null);
     const handleOptionClick = (eachOption) => {
         optionDisabled();
         if(eachOption.isCorrect){
             console.log('Right answer!');
+            optionRef.current.style.color = 'green';
         } else {
             console.log('wrong answer!');
+            optionRef.current.style.color = 'red';
         }
     }
     return(
         <div className={classNames} key={index}
              onClick={() => handleOptionClick(eachOption)}>
-            <div className="answer-option">{eachOption.optionText}</div>
+            <div className="answer-option" ref={optionRef}>{eachOption.optionText}</div>
         </div>
     )
 }
